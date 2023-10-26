d53c32e152de74e1fcd7574e2512c7614725f02a
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-26 12:40:29 +0700

    Add files via upload

---
+++ src/latihan26/Latihan26.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan26;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Dapatkan waktu saat ini
+        LocalDateTime waktuSaatIni = LocalDateTime.now();
+        Locale locale = new Locale("id", "ID");
+        
+        // format waktu
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMM yyyy HH:mm:ss", locale);
+        
+        // Tampilkan waktu
+        System.out.println("Hari ini adalah hari : " + formatter.format(waktuSaatIni));
+        
+    }
+    
+}
